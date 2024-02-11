1fa9727ef9a72c0dbecbcd8aa886b7c02c387f1f
Anton Harbers <Anton.Harbers23@gmail.com>
2024-02-11 23:46:32 +0100

    Feat: basic submit username form on game end, leaderboard data fetching

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -20,6 +20,9 @@ export default function GamePage() {
   const [selectedCoords, setSelectedCoords] = useState([0, 0]);
   const [selectedScreenCoords, setSelectedScreenCoords] = useState([0, 0]);
   const [markers, setMarkers] = useState([]);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+  const [nameText, setNameText] = useState('');
 
   const HandleImageClick = (event) => {
     const target = event.target;
@@ -67,8 +70,8 @@ export default function GamePage() {
 
       if (response.ok) {
         const data = await response.json();
-        console.log(data.score / 1000);
-        navigate('/leaderboards');
+        setGameOver(true);
+        setScore(data.score);
       } else {
         console.log('There was an issue with the server');
       }
@@ -77,6 +80,29 @@ export default function GamePage() {
     setSelectedScreenCoords([0, 0]);
     setIsSelecting(false);
     setSelectedCoords([0, 0]);
+  };
+
+  const SubmitNameForm = async () => {
+    // submit api request
+    const response = await fetch(
+      `${import.meta.env.VITE_API}/games/${gameId}/name`,
+      {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: nameText,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      navigate(`/level${level}leaderboard`);
+    } else {
+      console.log('problem uploading name to server');
+    }
   };
 
   const HandleStartGame = async () => {
@@ -103,9 +129,10 @@ export default function GamePage() {
     const startTime = Date.now(); // Capture start time in milliseconds
     const interval = setInterval(() => {
       // Calculate elapsed time in seconds
-      const secondsElapsed = Math.floor((Date.now() - startTime) / 1000);
+      const secondsElapsed = (Date.now() - startTime) / 1000;
+
       setTimer(secondsElapsed);
-    }, 1000); // Update every second
+    }, 10); // Update every second
 
     return () => clearInterval(interval);
   }, []);
@@ -121,50 +148,73 @@ export default function GamePage() {
     fetchDone.current = true;
   }, [level]);
 
-  return (
-    <div
-      onClick={() => {
-        isSelecting && setIsSelecting(false);
-      }}
-      className=" 
+  if (gameOver) {
+    return (
+      <div
+        className=" 
       select-none flex bg-slate-500 
       w-full h-[100vh] items-center justify-center 
-      text-center"
-    >
-      <h1 className="absolute top-5 ml-auto mr-auto">{timer.toFixed(2)}</h1>
-      {markers.map((marker) => (
-        <div
-          key={marker[0] + marker[1]}
-          style={{
-            position: 'absolute',
-            left: `${marker[0] + 15}px`,
-            top: `${marker[1] + 15}px`,
-          }}
-          className=" bg-green-500 h-5 w-5"
-        ></div>
-      ))}
-      <img
-        className=" h-[60vh] md:h-[80vh] w-auto max-w-[90vw] border-white border-4 rounded-sm"
-        src={gameImage}
-        alt="Game Image"
-        onClick={HandleImageClick}
-        onLoad={HandleStartGame}
-      />
-      {isSelecting && (
-        <div
-          style={{
-            position: 'absolute',
-            left: `${selectedScreenCoords[0]}px`,
-            top: `${selectedScreenCoords[1]}px`,
-          }}
-        >
-          <CharacterSelect
-            characterInfo={charData}
-            setCharacterInfo={setCharData}
-            HandleCharacterSelection={HandleCharacterSelection}
-          />
-        </div>
-      )}
-    </div>
-  );
+      text-center flex-col"
+      >
+        <h1>Score: {score}</h1>
+        <label htmlFor="name">Enter your name:</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Leaderboard name..."
+          value={nameText}
+          onChange={(e) => setNameText(e.target.value)}
+        />
+        <button onClick={SubmitNameForm}>Submit</button>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        onClick={() => {
+          isSelecting && setIsSelecting(false);
+        }}
+        className=" 
+        select-none flex bg-slate-500 
+        w-full h-[100vh] items-center justify-center 
+        text-center"
+      >
+        <h1 className="absolute top-5 ml-auto mr-auto">{timer.toFixed(2)}</h1>
+        {markers.map((marker) => (
+          <div
+            key={marker[0] + marker[1]}
+            style={{
+              position: 'absolute',
+              left: `${marker[0] + 15}px`,
+              top: `${marker[1] + 15}px`,
+            }}
+            className=" bg-green-500 h-5 w-5"
+          ></div>
+        ))}
+        <img
+          className=" h-[60vh] md:h-[80vh] w-auto max-w-[90vw] border-white border-4 rounded-sm"
+          src={gameImage}
+          alt="Game Image"
+          onClick={HandleImageClick}
+          onLoad={HandleStartGame}
+        />
+        {isSelecting && (
+          <div
+            style={{
+              position: 'absolute',
+              left: `${selectedScreenCoords[0]}px`,
+              top: `${selectedScreenCoords[1]}px`,
+            }}
+          >
+            <CharacterSelect
+              characterInfo={charData}
+              setCharacterInfo={setCharData}
+              HandleCharacterSelection={HandleCharacterSelection}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function LeaderboardPage() {
   const level = useLocation().pathname.slice(5)[1];
+  const [gameData, setGameData] = useState([{ _id: 1, player_name: 'time' }]);
+  const fetchDone = useRef(false);
 
-  return <div>LeaderboardPage {level}</div>;
+  useEffect(() => {
+    if (fetchDone.current) return;
+    const fetchData = async () => {
+      const response = await fetch(
+        `${import.meta.env.VITE_API}/games/${level}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setGameData(data);
+    };
+    fetchData();
+    fetchDone.current = true;
+  }, [level]);
+
+  return (
+    <div className="flex items-center justify-center flex-col h-[100vh] gap-2">
+      {gameData.map((game) => (
+        <div
+          key={game._id}
+          className="flex flex-col gap-2 w-[300px] items-start"
+        >
+          <h1>
+            {((game.finish_time - game.start_time) / 1000).toFixed(2)}s /{' '}
+            {game.player_name}
+          </h1>
+        </div>
+      ))}
+    </div>
+  );
 }
